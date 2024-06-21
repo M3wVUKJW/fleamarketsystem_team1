82eace4959a662a1f8263d36f72090d9574ceb79
M3wVUKJW <hexishijing1@gmail.com>
2024-06-21 17:26:37 +0900

    Update Product.java

--- fleamarketsystem_team1/src/main/java/bean/Product.java
+++ fleamarketsystem_team1/src/main/java/bean/Product.java
@@ -1,3 +1,4 @@
+//6/21 17:23 定数・コンストラクタ・メソッドに「入金状況」に関する処理を追加しました。【石井】
 package bean;
 
 public class Product {
@@ -10,6 +11,7 @@ public class Product {
 	private int quantity; //商品個数
 	private String description; //商品説明文
 	private String selldate; //出品日
+	private String deposit_status;//入金状況
 
 	//② コンストラクタ定義
 
@@ -22,11 +24,12 @@ public class Product {
 		this.quantity = 0; //商品個数初期化
 		this.description = null; //商品説明文初期化
 		this.selldate = null; //出品日初期化
+		this.deposit_status = null;//入金状況初期方
 
 	}
 
 	//③④ 各フィールド変数のGet・setメソッド定義
-
+	//商品ID
 	public int getProductid() {
 		return productid;
 	}
@@ -35,6 +38,7 @@ public class Product {
 		this.productid = productid;
 	}
 
+	//ユーザーID
 	public int getUserid() {
 		return userid;
 	}
@@ -43,6 +47,7 @@ public class Product {
 		this.userid = userid;
 	}
 
+	//商品名
 	public String getName() {
 		return name;
 	}
@@ -51,6 +56,7 @@ public class Product {
 		this.name = name;
 	}
 
+	//商品価格
 	public int getPrice() {
 		return price;
 	}
@@ -58,7 +64,7 @@ public class Product {
 	public void setPrice(int price) {
 		this.price = price;
 	}
-
+	 //商品個数
 	public int getQuantity() {
 		return quantity;
 	}
@@ -67,6 +73,7 @@ public class Product {
 		this.quantity = quantity;
 	}
 
+	//商品説明文
 	public String getDescription() {
 		return description;
 	}
@@ -75,6 +82,7 @@ public class Product {
 		this.description = description;
 	}
 
+	//出品日
 	public String getSelldate() {
 		return selldate;
 	}
@@ -83,4 +91,11 @@ public class Product {
 		this.selldate = selldate;
 	}
 
+	/入金状況
+	public String getdeposit_status() {
+		return deposit_status;
+	}
+	public void setdeposit_status(String deposit_status) {
+		this.deposit_status = deposit_status;
+	}
 }
